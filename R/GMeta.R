6b6423ac166d87518a73412bffb362a77fb2a77f
28pro92 <28pkundu1992@gmail.com>
2018-10-10 15:52:24 -0400

    removed some .R files

--- R/GMeta.R
+++ R/GMeta.R
@@ -11,6 +11,7 @@
 #' @param ref_dat a data matrix containing all the distinct covariates across studies from a reference set of individuals. This is used for estimating joint distribution of the covariates. The data matrix must have the vector of ones as its first column. The column names of the data matrix should match the names of the covariates from the studies.
 #' @param model a description of the type of regression model; this is a character string naming the regression model. The current version is for "logistic" and "linear".
 #' @param variable_intercepts an optional logical (applicable only when the model is "logistic"); if TRUE, the intercepts of the true models for each of the studies are assumed to be different. Default is FALSE.
+#' @param initial_val an optional numeric vector containing initial values for the parameters. This is used as a starting value in the optimiziation. Default is NULL, in which the initial value is calculated by meta-analysis of each of the parameter estimates across studies.
 #' @param control an optional list containing the epsilon (numeric) and maxiter (numeric) needed for convergence of the algorithm. Default epsilon and maximum iterations are 1e-06 and 1000, respectively. For creating a control argument for GMeta, see \code{\link[GMeta]{GMeta.control}}.
 #' @details Generalized Meta-analysis (GMeta) is a tool that allows researchers to quickly build models for multivariate meta-analysis in the presence of disparate covariate information across studies. It is implemented based on mainly two input arguments:
 #' \itemize{
@@ -27,24 +28,215 @@
 #' \item{Res.var}{a numeric containing the residual variance of the maximal model when it is linear. It is calculated from the formula : \eqn{1 - \hat{\beta}_{GMeta}^Tvar(X)\hat{\beta}_{GMeta}} which is derived by assuming the outcomes to have unit variance. \eqn{var(X)} is calculated from reference data. Res.var is NA when the model is "logistic".}
 #' \item{iter}{a numeric containing the number of iterations used in the algorithm}
 #' \item{call}{the matched call}
-#' \cr The function \code{\link[GMeta]{GMeta.summary}} prints a summary of the results obtained from GMeta.
-#' \cr The function \code{\link[GMeta]{GMeta.plot}} plots the estimate of the parameter from each of the studies, the summary measure(GMeta estimate) and their confidence intervals. 
 #' @keywords Generalized Meta Analysis
 #' @references Tang, R., Kundu, P. and Chatterjee, N. (2017) Generalized Meta-Analysis for Multivariate Regression Models Across Studies with Disparate Covariate Information. \href{https://arxiv.org/abs/1708.03818}{arXiv:1708.03818v1 [stat.ME]}.
-#' @seealso \code{\link[GMeta]{GMeta.summary}}, \code{\link[GMeta]{study_info}}, \code{\link[GMeta]{reference_data}}, \code{\link[GMeta]{GMeta.plot}}.
+#' @seealso \code{\link[GMeta]{GMeta.summary}}.
 #' @examples
 #' # This example shows the GMeta implementation on a simulated data set for logistic regression
-#' data(reference_data)
-#' head(reference_data)
-#' data(study_info)
-#' head(study_info)
-#' model <- "logistic"
-#' # When the true intercept parameters of the studies are different.
-#' result_diff <- GMeta(study_info, reference_data, model, variable_intercepts = TRUE)
-#' print(result_diff)
-#' # When the true intercept parameters of the studies are same.
-#' result_same <- GMeta(study_info, reference_data, model)
-#' print(result_same)
+#' #####
+#' ### Basic setting
+#' #####
+#'d.X = 3 # number of covariates.
+#' mu = matrix(rep(0,d.X), nrow=d.X) # mean vector of the covariates.
+
+#' r1 = 0.3 # correlation coefficient of the covariates.
+#' r2 = 0.6
+#' r3 = 0.1
+#' Sigma = matrix( 
+#'   c(1, r1, r2,  
+#'     r1, 1, r3, 
+#'     r2, r3, 1), 
+#'   nrow=d.X, 
+#'   ncol=d.X) # covariance matrix of the covariates.
+
+
+#' beta.star = matrix(c(-1.2, log(1.3), log(1.3), log(1.3)),nrow = d.X+1) # beta.star
+#' #beta.star = matrix(c(-1.2, 0.26, 0.26, 0.26),nrow = d.X+1) # beta.star
+#' #beta.star = matrix(c(-3, 1, 2, 3),nrow = d.X+1) # beta.star
+
+#' n1 = 300 # sample size of the 1st data set.
+#' n2 = 500 # 2nd
+#' n3 = 1000 # 3rd
+
+#' n = 50
+#' sim=1
+#' set.seed(sim)
+# Generate the reference data set
+#' X.rf = mvrnorm(n = n, mu, Sigma)
+
+
+# Generate data set 1. m1 means model 1.
+#' X.m1 = mvrnorm(n = n1, mu, Sigma) # Generate the covariates.
+#' X.m1.1 = cbind(rep(1, n1), X.m1) # Add a column of 1's to X.m1.
+#' p.m1 = 1/(1+exp(-X.m1.1%*%beta.star)) # the vector of probabilities
+#' Y.m1 = rbinom(n1, size=1, p.m1) # the Bernoulli responses
+#' # print(p.m1[1])
+#' # print(mean(Y.m1))
+#' # print(mean(p.m1))
+
+#' # Generate data set 2. m1 means model 2.
+#' X.m2 = mvrnorm(n = n2, mu, Sigma)
+#' X.m2.1 = cbind(rep(1, n2), X.m2)
+#' p.m2 = 1/(1+exp(-X.m2.1%*%beta.star))
+#' Y.m2 = rbinom(n2, size=1, p.m2)
+
+# Generate data set 3. m1 means model 3.
+#' X.m3 = mvrnorm(n = n3, mu, Sigma)
+#' X.m3.1 = cbind(rep(1, n3), X.m3)
+#' p.m3 = 1/(1+exp(-X.m3.1%*%beta.star))
+#' Y.m3 = rbinom(n3, size=1, p.m3)
+
+#' #####
+#' ### Create data sets in the format of data frame.
+#' #####
+#' data.m1 = data.frame(Y=Y.m1, X.m1)
+#' data.m2 = data.frame(Y=Y.m2, X.m2)
+#' data.m3 = data.frame(Y=Y.m3, X.m3)
+# str(data.m1)
+
+
+
+#' #####
+#' ### Apply logistic regression with reduced models to the data sets 
+#' #####
+#' logit.m1 <- glm(Y ~ X1 + X2, data = data.m1, family = "binomial")
+#' # print(logit.m1)
+#' if(logit.m1$converged == FALSE)
+#' {
+#'   print("glm for logit.m1 is not convergent.")
+#'   next
+#' }
+
+#' logit.m2 <- glm(Y ~ X2 + X3, data = data.m2, family = "binomial")
+#' # print(logit.m2)
+#' if(logit.m2$converged == FALSE)
+#' {
+#'   print("glm for logit.m2 is not convergent.")
+#'   next
+#' }
+
+#' logit.m3 <- glm(Y ~ X1 + X3, data = data.m3, family = "binomial")
+#' # print(logit.m3)
+#' if(logit.m3$converged == FALSE)
+#' {
+#'   print("glm for logit.m3 is not convergent.")
+#'   next
+#' }
+
+
+#' #####
+#' ### Obtain the estimators of the parameters in the reduced models. 
+#' #####
+#' theta.m1 = logit.m1$coefficients
+#' theta.m2 = logit.m2$coefficients
+#' theta.m3 = logit.m3$coefficients
+
+
+
+#' #####
+#' ### Find the covariance matrix estimators for the reduced models
+#' #####
+
+
+
+#' #####
+#' # Basic notations for inputs
+#' #####
+
+#' K = 3 # Number of data sets
+
+#' A1 = c(1, 2) # index set A1, the indexes of the covariates of data set 1.
+#' A2 = c(2, 3) # index set A2
+#' A3 = c(1, 3) # index set A3
+
+
+#' X.m1.used = cbind(rep(1, n1), X.m1[, A1, drop=FALSE])
+#' X.m2.used = cbind(rep(1, n2), X.m2[, A2, drop=FALSE])
+#' X.m3.used = cbind(rep(1, n3), X.m3[, A3, drop=FALSE])
+#' # str(X.m1.used)
+#' # str(X.m2.used)
+#' # str(X.m3.used)
+
+#' ##### Find Sigma.m1
+
+#' T.1 = matrix(rep(0, (length(A1)+1)^2), nrow=length(A1)+1)
+#' T.2 = T.1
+
+#' for (i in 1:n1)
+#' {
+#' a = as.vector(exp(-X.m1.used[i, , drop=FALSE]%*%theta.m1))
+#'   T.1 = T.1 + (a/(1+a)^2) * (t(X.m1.used[i, , drop=FALSE])%*%X.m1.used[i, , drop=FALSE])
+#' }
+
+#' for (i in 1:n1)
+#' {
+#'   a = as.vector(1/( 1 + exp(-X.m1.used[i, , drop=FALSE]%*%theta.m1)))
+#'   T.2 = T.2 + (Y.m1[i]-a)^2 * (t(X.m1.used[i, , drop=FALSE])%*%X.m1.used[i, , drop=FALSE])
+#' }
+
+#' Sigma.m1 = solve(T.1)%*%T.2%*%solve(T.1) # This is actually Sigma.m1.n1. 
+
+#' ##### Find Sigma.m2
+
+#' T.1 = matrix(rep(0, (length(A2)+1)^2), nrow=length(A2)+1)
+#' T.2 = T.1
+
+#' for (i in 1:n2)
+#' {
+#'   a = as.vector(exp(-X.m2.used[i, , drop=FALSE]%*%theta.m2))
+#'   T.1 = T.1 + (a/(1+a)^2) * (t(X.m2.used[i, , drop=FALSE])%*%X.m2.used[i, , drop=FALSE])
+#' }
+
+#' for (i in 1:n2)
+#' {
+#'   a = as.vector(1/( 1 + exp(-X.m2.used[i, , drop=FALSE]%*%theta.m2)))
+#'   T.2 = T.2 + (Y.m2[i]-a)^2 * (t(X.m2.used[i, , drop=FALSE])%*%X.m2.used[i, , drop=FALSE])
+#' }
+
+#' Sigma.m2 = solve(T.1)%*%T.2%*%solve(T.1)
+
+
+#' ##### Find Sigma.m3
+
+#' T.1 = matrix(rep(0, (length(A3)+1)^2), nrow=length(A3)+1)
+#' T.2 = T.1
+
+#' for (i in 1:n3)
+#' {
+#'   a = as.vector(exp(-X.m3.used[i, , drop=FALSE]%*%theta.m3))
+#'   T.1 = T.1 + (a/(1+a)^2) * (t(X.m3.used[i, , drop=FALSE])%*%X.m3.used[i, , drop=FALSE])
+#' }
+
+#' for (i in 1:n3)
+#' {
+#'   a = as.vector(1/( 1 + exp(-X.m3.used[i, , drop=FALSE]%*%theta.m3)))
+#'   T.2 = T.2 + (Y.m3[i]-a)^2 * (t(X.m3.used[i, , drop=FALSE])%*%X.m3.used[i, , drop=FALSE])
+#' }
+
+#' Sigma.m3 = solve(T.1)%*%T.2%*%solve(T.1)
+
+
+
+
+
+
+
+#' names(theta.m1)=c("(Intercept)","Age","Height")
+#' names(theta.m2)=c("(Intercept)","Height", "Weight")
+#' names(theta.m3)=c("(Intercept)","Age", "Weight")
+
+###now put in the GMeta example
+
+#' study1 = list(Coeff=theta.m1,Covariance=Sigma.m1,Sample_size=n1)
+#' study2 = list(Coeff=theta.m2,Covariance=Sigma.m2,Sample_size=n2)
+#' study3 = list(Coeff=theta.m3,Covariance=Sigma.m3,Sample_size=n3)
+
+#' studies = list(study1,study2,study3)
+#' model = "logistic"
+
+#' reference = cbind(rep(1,n), X.rf)
+#' colnames(reference) = c("(Intercept)","Age","Height", "Weight")
+#' result.same = GMeta(studies, reference, model, initial_val = c(-1.2, log(1.3), log(1.3), log(1.3)))
 
 #' @author Prosenjit Kundu, Runlong Tang and Nilanjan Chatterjee.
 #' @import magic
@@ -221,7 +413,7 @@ GMeta <- function(study_info, ref_dat, model, variable_intercepts=FALSE, initial
           {
             for(j in estimates_in_which_studies_indices[[k]])
             {
-              if(is.null(study_estimates[[j]][[2]]) == F)
+              if(is.null(study_estimates[[j]][[2]]) == FALSE)
               {
                 index_cov <- which(names(study_estimates[[j]][[1]]) %in% names_wo_intercept[k]  == T)
                 weight <- 1/study_estimates[[j]][[2]][index_cov, index_cov]
@@ -244,13 +436,13 @@ GMeta <- function(study_info, ref_dat, model, variable_intercepts=FALSE, initial
         # End of Calculating initial value when the different_intercept is TRUE
         
         # Calculating initial value when the different_intercept is FALSE
-        if(different_intercept == F)
+        if(different_intercept == FALSE)
         {
           for(k in 1: length(names_wo_intercept))
           {
             for(j in estimates_in_which_studies_indices[[k]])
             {
-              if(is.null(study_estimates[[j]][[2]]) == F)
+              if(is.null(study_estimates[[j]][[2]]) == FALSE)
               {
                 index_cov <- which(names(study_estimates[[j]][[1]]) %in% names_wo_intercept[k]  == T)
                 weight <- 1/study_estimates[[j]][[2]][index_cov, index_cov]
@@ -271,7 +463,7 @@ GMeta <- function(study_info, ref_dat, model, variable_intercepts=FALSE, initial
           }
           for(j in 1:no_of_studies)
           {
-            if(is.null(study_estimates[[j]][[2]]) == F)
+            if(is.null(study_estimates[[j]][[2]]) == FALSE)
             {
               weight <- 1/study_estimates[[j]][[2]][1, 1]
               weight_sum <- weight_sum + weight
@@ -513,7 +705,7 @@ GMeta <- function(study_info, ref_dat, model, variable_intercepts=FALSE, initial
                       
                     }
                     
-                    if(different_intercept == F)
+                    if(different_intercept == FALSE)
                     {
                       beta_identity <- as.vector(beta_identity)
                       names(beta_identity) <- colnames(ref_dat)
